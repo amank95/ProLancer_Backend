193ec278df6fa0521af194442811ff0cc76f1bb4
aman <amankanojiya313718@gmail.com>
2025-08-08 23:47:16 +0530

    add backend running in server.js

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser())
 
+app.get('/', (req, res) => {
+  res.send('Backend is running 🚀');
+});
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/gigs', gigRoute);
